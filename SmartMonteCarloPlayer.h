1bbf23b514753c1079e4b9566df4ff3f3a6a4c13
jdmax15 <joel.maxwell15@gmail.com>
2026-04-11 13:14:56 +1000

    Discovered that getMove() is now going to need to be overridden, making inheritence redundant. About to change SmartMonteCarloPlayer to inherit from Player instead.

--- SmartMonteCarloPlayer.h
+++ SmartMonteCarloPlayer.h
@@ -23,17 +23,36 @@ class SmartMonteCarloPlayer: public MonteCarloPlayer {
 
 
 
-double MonteCarloPlayer::simulation(const Board& board) {
-    int simulation_runs = 100;
+double SmartMonteCarloPlayer::simulation(const Board& board) {
+    int simulation_runs = 200;
+    int max_depth = 50;
+
     double total_score = 0.0;
     vector<char> directions = {'u', 'd', 'l', 'r'};
 
     for (int i = 0; i < simulation_runs; i++) {
         Board tempBoard(board);
-        while (tempBoard.canMove() && !tempBoard.hasWon()) {
-            int randNum = rand() % directions.size();
-            tempBoard.makeMove(directions[randNum]);
+        int steps = 0;
+
+        while (tempBoard.canMove() && !tempBoard.hasWon() && steps < max_depth) {
+            char bestMove = directions[0];
+            int bestHeuristic = -1;
+
+            for (int j = 0; j < directions.size(); j++) {
+                Board trial(tempBoard);
+                trial.makeMove(directions[j]);
+
+            if (trial.getCurrentScore() == tempBoard.getCurrentScore() &&
+                trial.countEmptyCells() == tempBoard.countEmptyCells()) continue;
+
+                if (trial.heuristic2() > bestHeuristic) {
+                    bestHeuristic = trial.heuristic2();
+                    bestMove = directions[j];
+                }
+            }
+            tempBoard.makeMove(bestMove);
             tempBoard.addNewTile();
+            steps++;
         }
         total_score += tempBoard.getCurrentScore();
     }
